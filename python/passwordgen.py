1856389d1efa852a558fbd4215c1961c79c76c5f
Kaida <kaidadragonfly@gmail.com>
2014-09-17 13:30:39 -0700

    Run pylint on passwordgen.py and update for py3k

--- python/passwordgen.py
+++ python/passwordgen.py
@@ -1,47 +1,70 @@
 #!/usr/bin/env python
-from os import urandom
-from string import letters, digits, punctuation
-from optparse import OptionParser
+"""
+A random string generator, designed for easy generation of passwords.
 
-def passgen(alphabet, length = 10):
+Uses os.urandom as source of entropy.
+"""
+
+from os import urandom
+from string import ascii_letters as letters, digits, punctuation
+from argparse import ArgumentParser
+
+def passgen(alphabet, length):
+    """
+    Actually perform the random string generation.
+    """
     passw = ""
     while len(passw) < int(length):
-        c = urandom(1)
-        if (c in alphabet):
-            passw += c
+        char = urandom(1).decode('utf-8', errors='replace')
+
+        if char in alphabet:
+            passw += char
     return passw
 
-parser = OptionParser()
-parser.add_option("-l",
-                  "--letters",
-                  action="store_true",
-                  help="output letters in the password")
-parser.add_option("-n",
-                  "--numbers",
-                  action="store_true",
-                  help="output numbers in the password")
-parser.add_option("-p",
-                  "--punctuation",
-                  action="store_true",
-                  help="output punctuation in the password")
-parser.add_option("-s",
-                  "--spaces",
-                  action="store_true",
-                  help="output spaces in the password")
+def main():
+    """
+    Collect the arguments, and fire off the random generation with
+    appropriate parameters.
+    """
+    parser = ArgumentParser(description="Generate a random password.")
+    parser.add_argument("-l",
+                        "--letters",
+                        action="store_true",
+                        help="output letters in the password")
+    parser.add_argument("-n",
+                        "--numbers",
+                        action="store_true",
+                        help="output numbers in the password")
+    parser.add_argument("-p",
+                        "--punctuation",
+                        action="store_true",
+                        help="output punctuation in the password")
+    parser.add_argument("-s",
+                        "--spaces",
+                        action="store_true",
+                        help="output spaces in the password")
+    parser.add_argument("length",
+                        metavar='LENGTH',
+                        type=int,
+                        nargs='?',
+                        help="the length of the password",
+                        default=10)
 
-(opts, args) = parser.parse_args()
+    args = parser.parse_args()
 
-chars = ''
-if opts.letters:
-    chars += letters
-if opts.numbers:
-    chars += digits
-if opts.punctuation:
-    chars += punctuation
-if opts.spaces:
-    chars += ' '
+    chars = ''
+    if args.letters:
+        chars += letters
+    if args.numbers:
+        chars += digits
+    if args.punctuation:
+        chars += punctuation
+    if args.spaces:
+        chars += ' '
 
-ascii = ' ' + letters + digits + punctuation
-    
-print passgen(chars or ascii, *args)
+    ascii_chars = ' ' + letters + digits + punctuation
 
+    print(passgen(chars or ascii_chars, args.length))
+
+if __name__ == '__main__':
+    main()
