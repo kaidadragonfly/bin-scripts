5bd6995ce05245d1fe1dfc471119d9bcf98f7887
kaidadragonfly <kaidadragonfly@gmail.com>
2015-04-01 21:10:23 -0700

    Multiprocess git-rec

--- python/git_rec.py
+++ python/git_rec.py
@@ -4,36 +4,49 @@ Recursively walk the current directory looking for git repositories.
 
 Then run the specified git command on each directory.
 """
-from os import walk, chdir, getcwd
+from os import chdir, walk
 from sys import stdout, argv
+from multiprocessing import Process
 from sh import ErrorReturnCode # pylint: disable=no-name-in-module
 
 from git_utils import git, wrap
 
-if __name__ == '__main__':
-    stdout.write('Locating repositories...')
-    stdout.flush()
-    REPOS = []
-    for root, dirs, files in walk('.', topdown=True):
-        if '.git' in dirs:
-            REPOS.append(root)
-        dirs[:] = [d for d in dirs if not d[0] == '.']
-    print(' done.')
-
-    ROOT_DIR = getcwd()
-    for repo in REPOS:
-        try:
-            chdir(repo)
-            command = git(argv[1:])
-            if len(command) > 0:
-                # Empty string prevents printing the tuple in python2
-                print('')
-                print("in repo {}:".format(repo))
-                for line in command:
-                    print(wrap(line))
-                    stdout.flush()
-
-        except ErrorReturnCode as ex:
+def do_command(repo):
+    """
+    Execute the command in the given repository.
+    """
+    try:
+        chdir(repo)
+        command = git(argv[1:])
+        if len(command) > 0:
+            # Empty string prevents printing the tuple in python2
+            print('')
             print("in repo {}:".format(repo))
-            print(wrap('Command: "{}" failed!'.format(ex.full_cmd)))
-        chdir(ROOT_DIR)
+            for line in command:
+                print(wrap(line))
+                stdout.flush()
+
+    except ErrorReturnCode as ex:
+        print("in repo {}:".format(repo))
+        print(wrap('Command: "{}" failed!'.format(ex.full_cmd)))
+
+def main():
+    """
+    Main loop to dispatch workers.
+    """
+    # pylint: disable=not-callable
+    # Process is reported as not callable.
+
+    workers = []
+    for root, dirs, _ in walk('.', topdown=True):
+        if '.git' in dirs:
+            worker = Process(target=do_command, args=(root,))
+            worker.start()
+            workers.append(worker)
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
+    for worker in workers:
+        worker.join()
+
+if __name__ == '__main__':
+    main()
