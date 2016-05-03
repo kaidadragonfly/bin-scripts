5163b44117ad796d0fe294adca674bb5b006861f
kaidadragonfly <kaidadragonfly@gmail.com>
2016-05-03 10:48:02 -0700

    Don't copy .bash_local to new hosts

--- populate-host.sh
+++ populate-host.sh
@@ -2,4 +2,4 @@
 
 set -o verbose
 ssh-copy-id "$1"
-scp ~/.bashrc ~/.bash_aliases ~/.bash_exports ~/.bash_local ~/.bash_logout ~/.profile "$1":
+scp ~/.bashrc ~/.bash_aliases ~/.bash_exports ~/.bash_logout ~/.profile "$1":
