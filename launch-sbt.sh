64d14170021672fcd67b44d257bf537f1b06edc0
kaidadragonfly <kaidadragonfly@gmail.com>
2016-12-30 10:36:30 -0800

    Don't go to proj root with launch-sbt.sh

--- launch-sbt.sh
+++ launch-sbt.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd "$(proj-root)" || exit
-
 if [ -e "$(proj-root)/.sbt-opts" ]; then
     source "$(proj-root)/.sbt-opts"
 else
     SBT_OPTS=(-Xmx512M ${SBT_OPTS})
 fi
+
 export SBT_OPTS
 
 export PATH="${JAVA_HOME}/bin:${PATH}"
