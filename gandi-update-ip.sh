ea46e2d6a33e89b0cb463356c96444d12523d953
kaidadragonfly <kaidadragonfly@gmail.com>
2020-07-28 14:41:37 -0700

    Add error checking to domain setting

--- gandi-update-ip.sh
+++ gandi-update-ip.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-IPV4=$(curl https://v4.rojoma.com/ip/ 2>/dev/null)
-IPV6=$(curl https://v6.rojoma.com/ip/ 2>/dev/null)
+set -e
+
+IPV4=$(curl -f https://v4.rojoma.com/ip/ 2>/dev/null)
+IPV6=$(curl -f https://v6.rojoma.com/ip/ 2>/dev/null)
 
 curl -D- -XPUT -H "Content-Type: text/plain" \
-    -H"X-Api-Key: $GANDI_API_KEY" \
+    -H"X-Api-Key: ${GANDI_API_KEY}" \
     --data-binary @- \
     "https://dns.api.gandi.net/api/v5/zones/${GANDI_ZONE_UUID}/records" \
     << EOF
