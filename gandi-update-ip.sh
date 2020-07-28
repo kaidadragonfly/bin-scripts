9397078c53a32dbed0f72c261e2419d7e0bb8e0b
kaidadragonfly <kaidadragonfly@gmail.com>
2020-07-28 14:29:02 -0700

    Create script to update Gandi.net IP address

---
+++ gandi-update-ip.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+IPV4=$(curl https://v4.rojoma.com/ip/ 2>/dev/null)
+IPV6=$(curl https://v6.rojoma.com/ip/ 2>/dev/null)
+
+curl -D- -XPUT -H "Content-Type: text/plain" \
+    -H"X-Api-Key: $GANDI_API_KEY" \
+    --data-binary @- \
+    "https://dns.api.gandi.net/api/v5/zones/${GANDI_ZONE_UUID}/records" \
+    << EOF
+@ IN A $IPV4
+@ IN AAAA $IPV6
+EOF
