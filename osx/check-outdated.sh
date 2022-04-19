dbda6b44e10ba839988942ef05a425049a586a49
Kaida <kaidadragonfly@gmail.com>
2022-04-19 14:18:32 -0700

    Update check-outdated for M1 macs

--- osx/check-outdated.sh
+++ osx/check-outdated.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-/usr/local/bin/brew outdated > "$HOME"/.brew_outdated
+if [ -x /opt/homebrew/bin/brew ]; then
+    BREW=/opt/homebrew/bin/brew
+fi
+
+if [ -x /usr/local/bin/brew ]; then
+    BREW=/usr/local/bin/brew
+fi
+
+$BREW outdated > "$HOME"/.brew_outdated
